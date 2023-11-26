5476e46fab7dbfad5dfd398e02bda396ad924e7e
Giheon Do <dgh0001@naver.com>
2023-11-26 22:13:22 +0900

    initialized finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div class="flex flex-col w-screen h-screen justify-center items-center">
+      <a href="/">
+        <div class="text-5xl mb-12 font-bold hover:text-gray-400 transition ease-in-out delay-100">
+          KNU COMP Crawler
+        </div>
+      </a>
+      <div>
+        <a href="https://www.google.com">
+          <button class=" bg-orange-500 h-24 w-48 mr-5 rounded-2xl text-white font-bold text-3xl hover:bg-orange-400 transition ease-in-out delay-70">
+            Signin
+          </button>
         </a>
-      </header>
+        <a href="https://www.google.com">
+          <button class="bg-teal-500 h-24 w-48 ml-5 rounded-2xl text-white font-bold text-3xl hover:bg-teal-400 transition ease-in-out delay-70">
+            Signup
+          </button>
+        </a>
+      </div>
     </div>
   );
 }
