fc0e34e07b1602a2a97815fa7bc13215417e7636
Giheon Do <dgh0001@naver.com>
2023-11-29 14:46:11 +0900

    cookie added, credential added

--- src/api.js
+++ src/api.js
@@ -1,8 +1,19 @@
 import axios from "axios";
+import Cookie from "js-cookie";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1/",
-  withCredentials:true,
+  withCredentials: true,
 });
 
 export const getMe = () => instance.get("users/me").then((res) => res.data);
+
+export const signIn = ({ username, password }) => {
+  return instance
+    .post(
+      "users/signin",
+      { username, password },
+      { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }
+    )
+    .then((response) => response.data);
+};
