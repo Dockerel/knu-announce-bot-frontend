228dc8a64dc52b0bbbdd71571be6faf7aa722660
Giheon Do <dgh0001@naver.com>
2023-11-29 13:26:50 +0900

    useQuery changed

--- src/lib/useUser.js
+++ src/lib/useUser.js
@@ -5,6 +5,7 @@ export default function useUser() {
   const { isLoading, data, isError } = useQuery({
     queryKey: ["me"],
     queryFn: () => getMe(),
+    retry: false,
   });
   return {
     userLoading: isLoading,
