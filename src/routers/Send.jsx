bb924cdbbe2b9360eaad44e2343b6a71c3583ac7
Giheon Do <dgh0001@naver.com>
2023-12-04 22:41:47 +0900

    MyPage.jsx, Send.jsx

---
+++ src/routers/Send.jsx
@@ -0,0 +1,15 @@
+function Send() {
+    return (
+      <div className="flex flex-col w-screen h-screen justify-center items-center">
+        <span className="block font-bold text-5xl">Send</span>
+        <a href="/">
+          <span className="block text-2xl mt-5 text-gray-400 hover:text-gray-600 underline">
+            Back to Home &rarr;
+          </span>
+        </a>
+      </div>
+    );
+  }
+  
+  export default Send;
+  
