e6a821a8f3877ca5a4d60700ee89b76c99441e8a
Giheon Do <dgh0001@naver.com>
2023-12-10 21:30:47 +0900

    react-helmet for title setting added. title set

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
 export default function Root() {
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <Helmet>
+        <title>KNU COMP Crawler</title>
+      </Helmet>
+      <div>
+        <Header />
+        <Outlet />
+      </div>
+    </>
   );
 }
