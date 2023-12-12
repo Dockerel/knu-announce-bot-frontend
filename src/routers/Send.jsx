ea2e665645a4fc406fe95aef0605642bc58b2374
Giheon Do <dgh0001@naver.com>
2023-12-12 23:16:11 +0900

    Send.jsx layout

--- src/routers/Send.jsx
+++ src/routers/Send.jsx
@@ -1,15 +1,19 @@
 function Send() {
-    return (
-      <div className="flex flex-col w-screen h-screen justify-center items-center">
-        <span className="block font-bold text-5xl">Send</span>
-        <a href="/">
-          <span className="block text-2xl mt-5 text-gray-400 hover:text-gray-600 underline">
-            Back to Home &rarr;
-          </span>
-        </a>
+  return (
+    <div>
+      <div className="text-5xl mb-12 font-bold hover:text-gray-400 transition ease-in-out delay-100">
+        Send
       </div>
-    );
-  }
-  
-  export default Send;
-  
+      <div>
+        <button className=" bg-orange-500 h-24 w-48 mr-5 rounded-2xl text-white font-bold text-3xl hover:bg-orange-400 transition ease-in-out delay-70">
+          Today
+        </button>
+        <button className=" bg-teal-500 h-24 w-48 mr-5 rounded-2xl text-white font-bold text-3xl hover:bg-teal-400 transition ease-in-out delay-70">
+          The past 7 days
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Send;
