1545d59e1f1199950b14d8086e85b2a730caa74a
Giheon Do <dgh0001@naver.com>
2023-12-27 22:06:00 +0900

    send type changed from the past 7days to all

--- src/routers/Send.jsx
+++ src/routers/Send.jsx
@@ -1,15 +1,15 @@
 function Send() {
   return (
-    <div>
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
       <div className="text-5xl mb-12 font-bold hover:text-gray-400 transition ease-in-out delay-100">
         Send
       </div>
-      <div>
+      <div className="flex flex-row justify-center items-center">
         <button className=" bg-orange-500 h-24 w-48 mr-5 rounded-2xl text-white font-bold text-3xl hover:bg-orange-400 transition ease-in-out delay-70">
           Today
         </button>
         <button className=" bg-teal-500 h-24 w-48 mr-5 rounded-2xl text-white font-bold text-3xl hover:bg-teal-400 transition ease-in-out delay-70">
-          The past 7 days
+          All
         </button>
       </div>
     </div>
