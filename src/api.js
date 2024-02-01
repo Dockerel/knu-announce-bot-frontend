31526e085a1638c95e3db7487e89baa72c33eba6
Giheon Do <dgh0001@naver.com>
2024-02-02 08:28:16 +0900

    .

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie from "js-cookie";
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV==="development" ? "http://127.0.0.1:8000/api/v1/" : "https://knubot.onrender.com/api/v1/",
+  baseURL: process.env.NODE_ENV==="development" ? "http://127.0.0.1:8000/api/v1/" : "https://knubot.onrender.com/api/v1",
   withCredentials: true,
 });
 
